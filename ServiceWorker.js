530093afbf87fb2d118c4488b8c1fa4299f38183
MM-Jagdish <jagdish@metamersivetech.com>
2024-06-21 15:08:21 +0530

    First Push

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "SAHU-Gemini-1";
+const contentToCache = [
+    "Build/Gemini WebGL Build.loader.js",
+    "Build/Gemini WebGL Build.framework.js",
+    "Build/Gemini WebGL Build.data",
+    "Build/Gemini WebGL Build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
